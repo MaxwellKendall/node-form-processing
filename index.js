45d7827dcc23d35f4a69c6d9652d6bbf2a61e03e
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-02-22 13:27:08 -0500

    using ses for simple email with no attachments

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const nodemailer = require('nodemailer');
+const path = require('path');
 const express = require('express');
+const AWS = require('aws-sdk');
 const multer = require('multer')();
 
 const app = express();
+const port = 3000;
+
+AWS.config.update({ region: 'us-east-1'});
 
 // using cors
 app.use(function(req, res, next) {
@@ -11,7 +15,9 @@ app.use(function(req, res, next) {
     next();
 });
 
-const port = 3000
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./index.html"));
+});
 
 app.post('/send-email', multer.single('file'), (req, res) => {
     const {
@@ -23,9 +29,42 @@ app.post('/send-email', multer.single('file'), (req, res) => {
         details
     } = req.body;
 
-    console.log("HELLLOOOOO", req.body, req.file);
-
-    res.send('fun timez');
+    // Create sendEmail params 
+    const params = {
+        Destination: { /* required */
+            ToAddresses: ['info@ckendallart.com']
+        },
+        Message: { /* required */
+            Body: { /* required */
+                Text: {
+                    Charset: "UTF-8",
+                    Data: details
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: `Commission Request from ${name}`
+            }
+        },
+        Source: 'info@ckendallart.com', /* required */
+        ReplyToAddresses: [
+            email
+        /* more items */
+        ],
+    };
+  
+    // Create the promise and SES service object
+    new AWS.SES({apiVersion: '2010-12-01'})
+        .sendEmail(params)
+        .promise()
+            .then((data) => {
+                console.log(data.MessageId);
+                res.send('fun timez');
+            })
+            .catch((err) => {
+                console.error(err, err.stack);
+                res.send('error');
+            });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
