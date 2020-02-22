522ca650f64584ca1b53ffb0f4d641822700d92a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-02-22 14:04:34 -0500

    correctly passing attachment in email

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const path = require('path');
 const express = require('express');
+const memoryStorage = require('multer').memoryStorage();
+const upload = require('multer')({ storage: memoryStorage });
 const AWS = require('aws-sdk');
-const multer = require('multer')();
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = 3000;
 
-AWS.config.update({ region: 'us-east-1'});
-
-// using cors
+// allowing CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+AWS.config.update({ region: 'us-east-1' });
+
+// create Nodemailer SES transporter
+let transporter = nodemailer.createTransport({
+    SES: new AWS.SES({
+        apiVersion: '2010-12-01'
+    })
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./index.html"));
 });
 
-app.post('/send-email', multer.single('file'), (req, res) => {
+app.post('/send-email', upload.single('file'), (req, res) => {
     const {
         name,
         preferredContact,
@@ -29,42 +38,35 @@ app.post('/send-email', multer.single('file'), (req, res) => {
         details
     } = req.body;
 
-    // Create sendEmail params 
-    const params = {
-        Destination: { /* required */
-            ToAddresses: ['info@ckendallart.com']
+    const attachment = req.file;
+    // send some mail
+    transporter.sendMail(
+        {
+            from: 'info@ckendallart.com',
+            to: 'info@ckendallart.com',
+            subject: `Commission Request from ${name}`,
+            html: `
+                <h1>${name}'s Request for a Commission:</h1>
+                <h2>Preferred Contact: ${preferredContact}</h2>
+                <h2>Phone: ${phone}</h2>
+                <h2>Email: ${email}</h2>
+                <h2>Canvas Size: ${canvas}</h2>
+                <p>Additional Details: ${details}</p>
+            `,
+            attachments: [{
+                filename: attachment.originalname,
+                contentTransferEncoding: 'base64',
+                content: attachment.buffer
+            }]
         },
-        Message: { /* required */
-            Body: { /* required */
-                Text: {
-                    Charset: "UTF-8",
-                    Data: details
-                }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: `Commission Request from ${name}`
-            }
-        },
-        Source: 'info@ckendallart.com', /* required */
-        ReplyToAddresses: [
-            email
-        /* more items */
-        ],
-    };
-  
-    // Create the promise and SES service object
-    new AWS.SES({apiVersion: '2010-12-01'})
-        .sendEmail(params)
-        .promise()
-            .then((data) => {
-                console.log(data.MessageId);
-                res.send('fun timez');
-            })
-            .catch((err) => {
-                console.error(err, err.stack);
-                res.send('error');
-            });
+        (err, info) => {
+            console.log("err", err, info);
+            // console.log(info.envelope);
+            // console.log(info.messageId);
+        }
+    );
+
+    res.send('fun timez');
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
