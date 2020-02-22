b7b943435f40e1f4b1b699ad6aeba88d010784f1
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-02-22 12:11:37 -0500

    working api

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
 const nodemailer = require('nodemailer');
-const path = require('path');
-const express = require('express')
-const app = express()
-var cors = require('cors')
-app.use(express.json()) // for parsing application/json
+const express = require('express');
+const multer = require('multer')();
+
+const app = express();
+
+// using cors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
 const port = 3000
 
-app.get('/', (req, res) => {
-    // const {
-    //     name,
-    //     preferredContact,
-    //     budget,
-    //     size,
-    //     desc
-    // } = req.body;
+app.post('/send-email', multer.single('file'), (req, res) => {
+    const {
+        name,
+        preferredContact,
+        phone,
+        email,
+        canvas,
+        details
+    } = req.body;
 
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/confirmation', (req, res) => {
-    res.sendFile(path.join(__dirname, 'success.html'));    
-})
-
-app.get('/test', cors(), (req, res) => {
-    console.log("wut");
-})
-
-app.post('/send-email', cors(), (req, res) => {
-    // const {
-    //     name,
-    //     email,
-    //     details
-    // } = req.body;
-
-    console.log("HELLLOOOOO");
+    console.log("HELLLOOOOO", req.body, req.file);
 
     res.send('fun timez');
 })
